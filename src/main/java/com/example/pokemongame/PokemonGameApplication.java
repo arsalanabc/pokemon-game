2f0e9de7c2f4bae2429f71a04fb3dc38ed425d17
arsalan.ahmed <arsalanabc@gmail.com>
2022-06-14 22:41:48 -0400

    Handle invalid input

--- src/main/java/com/example/pokemongame/PokemonGameApplication.java
+++ src/main/java/com/example/pokemongame/PokemonGameApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriBuilder;
 import javax.crypto.spec.PSource;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,77 +33,83 @@ public class PokemonGameApplication {
 			pokemons.add(pokemon);
 		}
 		Game game = new Game(pokemons);
-		Scanner scanner = new Scanner(System.in);
 		showMenu();
 
 		while(true){
+			Scanner scanner = new Scanner(System.in);
 			System.out.println("Pick an option! 7 to show menu");
+			try {
+				int userInput = scanner.nextInt();
 
-			int s = scanner.nextInt();
-			if(s == 0) break;
+				if(userInput == 0) break;
 
-			switch (s){
-				case 1:
-					showPokemons(game);
-					break;
-				case 2:
-					System.out.println("Pick a Pokemon for player one!");
-					while(true){
+				switch (userInput){
+					case 1:
 						showPokemons(game);
-						int i = scanner.nextInt();
-						if(i < 0 || i >= game.getPokemons().size()) {
-							System.out.println("Invalid Pokemon. Please pick again!");
-						} else {
-							Pokemon p = game.getPokemons().get(i);
-							Player player1 = new Player("player_1", p);
-							game.setPlayerOne(player1);
-							System.out.println(p.getName()+" is picked for "+player1.getName());
-							break;
+						break;
+					case 2:
+						System.out.println("Pick a Pokemon for player one!");
+						while(true){
+							showPokemons(game);
+							int i = scanner.nextInt();
+							if(i < 0 || i >= game.getPokemons().size()) {
+								System.out.println("Invalid Pokemon. Please pick again!");
+							} else {
+								Pokemon p = game.getPokemons().get(i);
+								Player player1 = new Player("player_1", p);
+								game.setPlayerOne(player1);
+								System.out.println(p.getName()+" is picked for "+player1.getName());
+								break;
+							}
 						}
-					}
-					break;
-				case 3:
-					System.out.println("Pick a Pokemon for player two!");
-					while(true){
-						showPokemons(game);
-						int i = scanner.nextInt();
-						if(i < 0 || i >= game.getPokemons().size()) {
-							System.out.println("Invalid Pokemon. Please pick again!");
-						} else {
-							Pokemon p = game.getPokemons().get(i);
-							Player player = new Player("player_2", p);
-							game.setPlayerTwo(player);
-							System.out.println(p.getName()+" is picked for "+player.getName());
-							break;
+						break;
+					case 3:
+						System.out.println("Pick a Pokemon for player two!");
+						while(true){
+							showPokemons(game);
+							int i = scanner.nextInt();
+							if(i < 0 || i >= game.getPokemons().size()) {
+								System.out.println("Invalid Pokemon. Please pick again!");
+							} else {
+								Pokemon p = game.getPokemons().get(i);
+								Player player = new Player("player_2", p);
+								game.setPlayerTwo(player);
+								System.out.println(p.getName()+" is picked for "+player.getName());
+								break;
+							}
 						}
-					}
-					break;
-				case 4:
-					try {
-						game.startBattle();
-						System.out.println("Battle is over");
-					} catch (NullPointerException e){
-						System.out.println(e.getMessage());
-					}
-					break;
-				case 5:
-					try {
-						System.out.println("The winner is "+ game.getWinner().getName());
-					} catch (NullPointerException e){
-						System.out.println(e.getMessage());
-					}
-					break;
-				case 6:
-					game = new Game(pokemons);
-					break;
-				case 7:
-					showMenu();
-					break;
-				default:
-					System.out.println("Invalid entry");
-					break;
+						break;
+					case 4:
+						try {
+							game.startBattle();
+							System.out.println("Battle is over");
+						} catch (NullPointerException e){
+							System.out.println(e.getMessage());
+						}
+						break;
+					case 5:
+						try {
+							System.out.println("The winner is "+ game.getWinner().getName());
+						} catch (NullPointerException e){
+							System.out.println(e.getMessage());
+						}
+						break;
+					case 6:
+						game = new Game(pokemons);
+						break;
+					case 7:
+						showMenu();
+						break;
+					default:
+						System.out.println("Invalid entry");
+						break;
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid entry");
 			}
 		}
+
 
 		// SpringApplication.run(PokemonGameApplication.class, args);
 	}
